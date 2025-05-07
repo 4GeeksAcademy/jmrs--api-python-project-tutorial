c8f03e0701dde1792ee9e6b7e4cda2dfe3dde17f
JMRS18 <juanmreverons@gmail.com>
2025-05-07 21:17:54 +0000

    API

--- src/app.py
+++ src/app.py
@@ -3,8 +3,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from dotenv import load_dotenv
 
+install spotipy
+
 # load the .env file variables
 load_dotenv()
 
 
 # Spotify API credentials
+client_id = os.environ.get()
